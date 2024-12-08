ed54ad82bcbd5e6c2c7b0ae9143b429c645f4272
CppCXY <812125110@qq.com>
2024-12-08 18:03:40 +0800

    support set_ls_locale

--- crates/emmylua_ls/src/handlers/initialized/locale.rs
+++ crates/emmylua_ls/src/handlers/initialized/locale.rs
@@ -6,7 +6,6 @@ pub fn set_ls_locale(params: &InitializeParams) -> Option<()> {
     let locale: String = params.locale.clone()?;
 
     info!("set locale: {}", locale);
-    code_analysis::set_locale("zh_CN");
-    // code_analysis::set_locale(&locale);
+    code_analysis::set_locale(&locale);
     Some(())
 }
