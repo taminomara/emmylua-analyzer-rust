a2d40516a54a028e6c541dc874b92baadaec356c
CppCXY <812125110@qq.com>
2025-02-17 20:33:41 +0800

    add more test case

--- crates/emmylua_code_analysis/src/semantic/type_check/test.rs
+++ crates/emmylua_code_analysis/src/semantic/type_check/test.rs
@@ -22,6 +22,27 @@ mod test {
     }
 
     #[test]
+    fn test_number_types() {
+        let mut ws = VirtualWorkspace::new();
+
+        let number_ty = ws.ty("number");
+        let integer_ty = ws.ty("integer");
+
+        let number_expr1 = ws.expr_ty("1");
+        assert!(ws.check_type(&number_ty, &number_expr1));
+        let number_expr2 = ws.expr_ty("1.5");
+        assert!(ws.check_type(&number_ty, &number_expr2));
+
+        
+        assert!(ws.check_type(&number_ty, &integer_ty));
+        assert!(!ws.check_type(&integer_ty, &number_ty));
+
+        let number_union = ws.ty("1 | 2 | 3");
+        assert!(ws.check_type(&number_ty, &number_union));
+        assert!(ws.check_type(&integer_ty, &number_union));
+    }
+
+    #[test]
     fn test_union_types() {
         let mut ws = VirtualWorkspace::new();
 
@@ -40,17 +61,78 @@ mod test {
         assert!(ws.check_type(&ty_union2, &ty_string));
         assert!(ws.check_type(&ty_union2, &ty_union));
         assert!(ws.check_type(&ty_union2, &ty_union2));
+
+        let ty_union3 = ws.ty("1 | 2 | 3");
+        let ty_union4 = ws.ty("1 | 2");
+
+        assert!(ws.check_type(&ty_union3, &ty_union4));
+        assert!(!ws.check_type(&ty_union4, &ty_union3));
+        assert!(ws.check_type(&ty_union3, &ty_union3));
     }
 
     #[test]
     fn test_object_types() {
         let mut ws = VirtualWorkspace::new();
 
-        let ty_table = ws.ty("{ x: number, y: string }");
-        let ty_match = ws.ty("{ x: 1, y: 'test' }");
-        let ty_mismatch = ws.ty("{ x: 2, y: 3 }");
+        // case 1
+        {
+            let object_ty = ws.ty("{ x: number, y: string }");
+            let matched_object_ty2 = ws.ty("{ x: 1, y: 'test' }");
+            let mismatch_object_ty2 = ws.ty("{ x: 2, y: 3 }");
+            let matched_table_ty = ws.expr_ty("{ x = 1, y = 'test' }");
+            let mismatch_table_ty = ws.expr_ty("{ x = 2, y = 3 }");
 
-        assert!(ws.check_type(&ty_table, &ty_match));
-        assert!(!ws.check_type(&ty_table, &ty_mismatch));
+            assert!(ws.check_type(&object_ty, &matched_object_ty2));
+            assert!(!ws.check_type(&object_ty, &mismatch_object_ty2));
+            assert!(ws.check_type(&object_ty, &matched_table_ty));
+            assert!(!ws.check_type(&object_ty, &mismatch_table_ty));
+        }
+
+        // case for tuple, object, and table
+        {
+            let object_ty = ws.ty("{ [1]: string, [2]: number }");
+            let matched_tulple_ty = ws.ty("[string, number");
+            let matched_object_ty = ws.ty("{ [1]: 'test', [2]: 1 }");
+
+            assert!(ws.check_type(&object_ty, &matched_tulple_ty));
+            assert!(ws.check_type(&object_ty, &matched_object_ty));
+            let mismatch_tulple_ty = ws.ty("[number, string]");
+            assert!(!ws.check_type(&object_ty, &mismatch_tulple_ty));
+
+            let matched_table_ty = ws.expr_ty("{ [1] = 'test', [2] = 1 }");
+            assert!(ws.check_type(&object_ty, &matched_table_ty));
+        }
+    }
+
+    #[test]
+    fn test_array_types() {
+        let mut ws = VirtualWorkspace::new();
+
+        let array_ty = ws.ty("number[]");
+        let matched_tuple_ty = ws.ty("[1, 2, 3]");
+        let mismatch_array_ty = ws.ty("['a', 'b', 'c']");
+
+        assert!(ws.check_type(&array_ty, &matched_tuple_ty));
+        assert!(!ws.check_type(&array_ty, &mismatch_array_ty));
+
+        let array_ty2 = ws.ty("integer[]");
+        assert!(ws.check_type(&array_ty, &array_ty2));
+        assert!(!ws.check_type(&array_ty2, &array_ty));
+    }
+
+    #[test]
+    fn test_tuple_types() {
+        let mut ws = VirtualWorkspace::new();
+
+        let tuple_ty = ws.ty("[number, string]");
+        let matched_tuple_ty = ws.ty("[1, 'test']");
+        let mismatch_tuple_ty = ws.ty("['a', 1]");
+
+        assert!(ws.check_type(&tuple_ty, &matched_tuple_ty));
+        assert!(!ws.check_type(&tuple_ty, &mismatch_tuple_ty));
+
+        let tuple_ty2 = ws.ty("[integer, string]");
+        assert!(ws.check_type(&tuple_ty, &tuple_ty2));
+        assert!(!ws.check_type(&tuple_ty2, &tuple_ty));
     }
 }
