96b22f71814b8fa4bc0cb0fe350cadac85b6f9a2
xuhuanzy <501417909@qq.com>
2025-03-04 08:18:23 +0800

    diagnostic: optimize missing_parameter

--- crates/emmylua_code_analysis/src/diagnostic/checker/missing_parameter.rs
+++ crates/emmylua_code_analysis/src/diagnostic/checker/missing_parameter.rs
@@ -76,10 +76,10 @@ fn check_call_expr(
                 DiagnosticCode::MissingParameter,
                 right_paren.get_range(),
                 t!(
-                    "expected %{num} but found %{found_num}.\n%{infos}",
+                    "expected %{num} parameters but found %{found_num}. %{infos}",
                     num = params.len(),
                     found_num = args_count,
-                    infos = miss_parameter_info.join("\n")
+                    infos = miss_parameter_info.join(" \n ")
                 )
                 .to_string(),
                 None,
