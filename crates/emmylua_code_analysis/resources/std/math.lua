adfbc762e471d8443ae4e4ead189a9e19fec08ba
CppCXY <812125110@qq.com>
2025-03-06 20:11:21 +0800

    refactor math lib annotation

--- crates/emmylua_code_analysis/resources/std/math.lua
+++ crates/emmylua_code_analysis/resources/std/math.lua
@@ -18,21 +18,22 @@ math = {}
 
 ---
 --- Returns the absolute value of `x`. (integer/float)
+---@overload fun(x:integer):integer
 ---@param x number
 ---@return number
-function math.abs(x) return 0 end
+function math.abs(x) end
 
 ---
 --- Returns the arc cosine of `x` (in radians).
 ---@param x number
 ---@return number
-function math.acos(x) return 0 end
+function math.acos(x) end
 
 ---
 --- Returns the arc sine of `x` (in radians).
 ---@param x number
 ---@return number
-function math.asin(x) return 0 end
+function math.asin(x) end
 
 ---
 --- Returns the arc tangent of `y/x` (in radians), but uses the signs of both
@@ -45,25 +46,25 @@ function math.asin(x) return 0 end
 ---@param y number
 ---@param x number
 ---@return number
-function math.atan(y, x) return 0 end
+function math.atan(y, x) end
 
 ---
 --- Returns the smallest integer larger than or equal to `x`.
 ---@param x number
----@return number
-function math.ceil(x) return 0 end
+---@return integer
+function math.ceil(x) end
 
 ---
 --- Returns the cosine of `x` (assumed to be in radians).
 ---@param x number
 ---@return number
-function math.cos(x) return 0 end
+function math.cos(x) end
 
 ---
 --- Converts the angle `x` from radians to degrees.
 ---@param x number
 ---@return number
-function math.deg(x) return 0 end
+function math.deg(x) end
 
 ---
 --- Returns the value *e^x* (where e is the base of natural logarithms).
@@ -74,7 +75,7 @@ function math.exp(x) end
 ---
 --- Returns the largest integer smaller than or equal to `x`.
 ---@param x number
----@return number
+---@return integer
 function math.floor(x) end
 
 ---
@@ -102,6 +103,7 @@ function math.log(x, base) end
 ---
 --- Returns the argument with the maximum value, according to the Lua operator
 --- `<`. (integer/float)
+---@overload fun(x:integer, ...:integer):integer
 ---@param x number
 ---@param ... number
 ---@return number
@@ -109,12 +111,13 @@ function math.max(x, ...) end
 
 ---
 --- An integer with the maximum value for an integer.
----@type number
+---@type integer
 math.maxinteger = nil
 
 ---
 --- Returns the argument with the minimum value, according to the Lua operator
 --- `<`. (integer/float)
+---@overload fun(x:integer, ...:integer):integer
 ---@param x number
 ---@param ... number
 ---@return number
@@ -122,14 +125,14 @@ function math.min(x, ...) end
 
 ---
 --- An integer with the minimum value for an integer.
----@type number
+---@type integer
 math.mininteger = nil
 
 ---
 --- Returns the integral part of `x` and the fractional part of `x`. Its second
 --- result is always a float.
 ---@param x number
----@return number
+---@return integer, number
 function math.modf(x) end
 
 ---
@@ -149,15 +152,15 @@ function math.rad(x) end
 --- in the range *[m, n]*. The call `math.random(n)` is equivalent to `math
 --- .random`(1,n).
 ---@overload fun():number
----@param m? number
----@param n? number
----@return number
+---@param m integer
+---@param n? integer
+---@return integer
 function math.random(m, n) end
 
 ---
 --- Sets `x` as the "seed" for the pseudo-random generator: equal seeds
 --- produce equal sequences of numbers.
----@param x number
+---@param x integer
 function math.randomseed(x) end
 
 ---
@@ -183,7 +186,7 @@ function math.tan(x) return 0 end
 --- If the value `x` is convertible to an integer, returns that integer.
 --- Otherwise, returns `nil`.
 ---@param x number
----@return number
+---@return integer
 function math.tointeger(x) end
 
 ---
