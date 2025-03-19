e5a271841d0d75f42c7eaf3ff7d02827e57c71c3
CppCXY <812125110@qq.com>
2025-03-19 11:36:39 +0800

    add test for assert return, fix #204

--- crates/emmylua_code_analysis/src/compilation/test/multi_return.rs
+++ crates/emmylua_code_analysis/src/compilation/test/multi_return.rs
@@ -38,4 +38,26 @@ mod test {
         assert_eq!(b_ty, b_expected);
         assert_eq!(c_ty, c_expected);
     }
+
+    #[test]
+    fn test_assert_return() {
+        let mut ws = VirtualWorkspace::new_with_init_std_lib();
+
+        ws.def(
+            r#"
+        ---@return string?
+        ---@return string?
+        function cwd() end
+
+        a, b = assert(cwd())
+        "#,
+        );
+
+        let a_ty = ws.expr_ty("a");
+        let b_ty = ws.expr_ty("b");
+        let a_expected = ws.ty("string");
+        let b_expected = ws.ty("string?");
+        assert_eq!(a_ty, a_expected);
+        assert_eq!(b_ty, b_expected);
+    }
 }
