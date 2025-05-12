02122815364281cb5fa4b76b130e147e56866521
Komo <afiqquraisyzulkarnain@gmail.com>
2025-05-12 16:21:36 +0800

    ci: push version bump on Cargo.toml

--- flake.nix
+++ flake.nix
@@ -3,58 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
+      self,
       nixpkgs,
-      rust-overlay,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ rust-overlay.overlays.default ];
         pkgs = import nixpkgs { inherit system overlays; };
-
-        inherit (pkgs) rustPlatform;
       in
       {
         packages =
           let
-            mkPackage =
-              x:
-              let
-                cargoToml = builtins.fromTOML (builtins.readFile ./crates/${x}/Cargo.toml);
-              in
-              rustPlatform.buildRustPackage {
-                pname = cargoToml.package.name;
-                version = cargoToml.package.version;
-
-                src = ./.;
-                cargoLock.lockFile = ./Cargo.lock;
-
-                nativeBuildInputs = with pkgs; [ pkg-config ];
-                buildInputs = with pkgs; [ stdenv.cc.cc.lib ];
-
-                buildAndTestSubdir = "crates/${x}";
-
-                postFixup = ''
-                  patchelf --set-rpath "${pkgs.stdenv.cc.cc.lib}/lib" $out/bin/${x}
-                '';
-              };
+            packages = import ./nix/packages.nix;
           in
-          rec {
-            emmylua_ls = mkPackage "emmylua_ls";
-            emmylua_doc_cli = mkPackage "emmylua_doc_cli";
-            emmylua_check = mkPackage "emmylua_check";
-            default = emmylua_ls;
+          (builtins.mapAttrs (name: value: pkgs.callPackage value { }) packages)
+          // {
+            default = self.packages.${system}.emmylua_ls;
           };
       }
     );
