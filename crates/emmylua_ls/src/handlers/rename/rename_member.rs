65b6c5bd5d84e6774b6b0ce3b54f22cfeb5d9b34
xuhuanzy <501417909@qq.com>
2025-05-30 04:41:31 +0800

    允许 rename 字段时可以从表影响所有
    ```lua
    ---@class T
    ---@field abc fun(self: T) 注释注释
    
    ---@type T
    local t = {
        abc = function(self) -- 在此重命名 abc 可以影响所有引用
        end,
    }
    t.abc = function(self)
    end
    ```

--- crates/emmylua_ls/src/handlers/rename/rename_member.rs
+++ crates/emmylua_ls/src/handlers/rename/rename_member.rs
@@ -6,6 +6,8 @@ use emmylua_code_analysis::{
 use emmylua_parser::{LuaAst, LuaAstNode, LuaAstToken, LuaNameToken, LuaSyntaxNode};
 use lsp_types::Uri;
 
+use crate::handlers::hover::find_member_origin_owner;
+
 pub fn rename_member_references(
     semantic_model: &SemanticModel,
     compilation: &LuaCompilation,
@@ -23,7 +25,8 @@ pub fn rename_member_references(
         .get_reference_index()
         .get_index_references(&key)?;
 
-    let property_owner = LuaSemanticDeclId::Member(member_id);
+    let property_owner = find_member_origin_owner(semantic_model, member_id)
+        .unwrap_or(LuaSemanticDeclId::Member(member_id));
     let mut semantic_cache = HashMap::new();
     for in_filed_syntax_id in index_references {
         let semantic_model =
