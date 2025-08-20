1b22ca7ae75d4f0bd662b71bda9b9f1c2c0a2c43
CppCXY <812125110@qq.com>
2025-08-20 17:07:00 +0800

    support protobuf highlight

--- crates/emmylua_parser_desc/src/lang/mod.rs
+++ crates/emmylua_parser_desc/src/lang/mod.rs
@@ -1,5 +1,6 @@
 mod json;
 mod lua;
+mod protobuf;
 mod shell;
 mod sql;
 mod vimscript;
@@ -10,8 +11,8 @@ use crate::{
     DescItemKind,
     lang::{
         json::process_json_code_block, lua::process_lua_code_block,
-        shell::process_shell_code_block, sql::process_sql_code_block,
-        vimscript::process_vimscript_code_block,
+        protobuf::process_protobuf_code_block, shell::process_shell_code_block,
+        sql::process_sql_code_block, vimscript::process_vimscript_code_block,
     },
     util::ResultContainer,
 };
@@ -24,6 +25,7 @@ pub enum CodeBlockLang {
     Json,
     Shell,
     Sql,
+    Protobuf,
     Other,
 }
 
@@ -36,6 +38,7 @@ impl CodeBlockLang {
             "json" | "Json" => Some(CodeBlockLang::Json),
             "shell" | "Shell" => Some(CodeBlockLang::Shell),
             "sql" | "Sql" => Some(CodeBlockLang::Sql),
+            "Protobuf" | "protobuf" => Some(CodeBlockLang::Protobuf),
             _ => Some(CodeBlockLang::Other),
         }
     }
@@ -54,6 +57,7 @@ pub fn process_code<'a, C: ResultContainer>(
         CodeBlockLang::Json => process_json_code_block(c, reader, state),
         CodeBlockLang::Shell => process_shell_code_block(c, reader, state),
         CodeBlockLang::Sql => process_sql_code_block(c, reader, state),
+        CodeBlockLang::Protobuf => process_protobuf_code_block(c, reader, state),
         _ => {
             c.emit_range(range, DescItemKind::CodeBlock);
             return state;
